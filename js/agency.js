34f030c2f76fcaf98006b37883cccfb3b54ce40e
dlucas17 <39320034+dlucas17@users.noreply.github.com>
2018-06-04 15:37:36 +0200

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -48,23 +48,23 @@
   })
 
 //CAROUSEL
-$('#recipeCarousel').carousel({
-  interval: 10000
+$('#theCarousel').carousel({
+  interval: 2000
 })
 
-$('.carousel .carousel-item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
     next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    if (next.next().length>0) {
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
     next.next().children(':first-child').clone().appendTo($(this));
-    }
-    else {
-      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
 });
   
 })(jQuery); // End of use strict
