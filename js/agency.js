142eb3a3fab16e73c984a79cb4e328148ed56094
dlucas17 <39320034+dlucas17@users.noreply.github.com>
2018-06-05 23:09:30 +0200

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -76,5 +76,17 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
     }
 });
   
+var button = document.getElementById('button'); // Assumes element with id='button'
+
+//toggle divs  
+button.onclick = function() {
+    var div = document.getElementById('newpost');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};  
   
 })(jQuery); // End of use strict
